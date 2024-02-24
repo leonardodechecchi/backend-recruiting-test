f051720747a96f9ba5f52b4a172d0e4f5c451507
Leonardo <leo.dechi99@gmail.com>
2024-02-24 17:04:01 +0100

    Removed DogStatus type

--- src/models/Dog.ts
+++ src/models/Dog.ts
@@ -1,16 +1,14 @@
 import { ObjectId } from 'mongodb';
 
-type DogStatus = 'available' | 'adopted' | 'in-custody';
-
 class Dog {
   constructor(
     public name: string,
     public breed: string,
     public age: number,
-    public status: DogStatus,
+    public status: 'available' | 'adopted' | 'in-custody',
     public adoptionDate: Date,
     public id?: ObjectId
   ) {}
 }
 
-export { Dog, DogStatus };
+export { Dog };
